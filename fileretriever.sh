0faf50ba0ee926cb8f65163874f693f02977a871
Bastijea <bastien.jeannelle@lendico.de>
2020-09-14 10:07:04 +0200

    Working cleaner structure

--- fileretriever.sh
+++ fileretriever.sh
@@ -2,7 +2,7 @@
 
 FILES=$(ssh bastibast@192.168.0.200 -- find /home/bastibast/lendicotest -type f -mtime 0 2>&1) 
 echo "$FILES"
-scp -T -i ~/.ssh/id_rsa bastibast@192.168.0.200:"'$FILES'" ~/lendico/multiplecopy/${FILES##*/}
+scp -T -i ~/.ssh/id_rsa bastibast@192.168.0.200:"$FILES" ~/lendico/multiplecopy/${FILES##*/}
 
 
 
